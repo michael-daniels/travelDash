f818ae9878c65b5115d2553f67abac88726a0e22
Joe Sando <joesando@Joes-MacBook-Pro-2.local>
2018-03-07 14:40:48 -0700

    joe update 12

--- controllers/flight.js
+++ controllers/flight.js
@@ -3,7 +3,8 @@ const knex = require("../db/knex.js");
 module.exports = {
   // CHANGE ME TO AN ACTUAL FUNCTION
   displayFlight: function(req, res) {
-    knex('flight').then((results)=>{
+    knex('flight').where('id', 'trips.flight_id').then((results)=>{
+      
       res.render('trips', {flight:results, user_info: req.session.user})
     })
 
